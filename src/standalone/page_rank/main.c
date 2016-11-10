b1aadbc7c6b6d81a6dd0bfcecb2b3a8722f12c36
eunaeuna <euna.kim@gateh.edu>
2016-11-10 16:43:08 -0500

    pr[] initialization, OMP removed, test code added

--- src/standalone/page_rank/main.c
+++ src/standalone/page_rank/main.c
@@ -4,7 +4,7 @@
 #define _FILE_OFFSET_BITS 64
 
 #if defined(_OPENMP)
-#include "omp.h"
+//#include "omp.h"
 #endif
 
 #include "stinger_core/stinger_atomics.h"
@@ -20,15 +20,20 @@
 
 #define EPSILON_DEFAULT 1e-8
 #define DAMPINGFACTOR_DEFAULT 0.85
-#define MAXITER_DEFAULT 50 //20
+#define MAXITER_DEFAULT 20 //50 //20
 
-#define NO_ITERATION_LIMIT 1
-#define COUNT_NUM_V_DELTA  1
-#define ADD_GRAPH_UPDATE   1
+#define NO_ITERATION_LIMIT 0
+#define COUNT_NUM_V_DELTA  0
+#define ADD_GRAPH_UPDATE   0
+#define SAVE_PR_VALUES     0
+#define TEST_EPSLION_THRESHOLD 1
 
 int64_t page_rank_subset(stinger_t * S, int64_t NV, uint8_t * vertex_set, int64_t vertex_set_size, double * pr, double * tmp_pr_in, double epsilon, double dampingfactor, int64_t maxiter);
-//int64_t page_rank_directed(stinger_t * S, int64_t NV, double * pr, double * tmp_pr_in, double epsilon, double dampingfactor, int64_t maxiter);
-int64_t page_rank_directed(stinger_t * S, int64_t NV, double * pr, double * tmp_pr_in, double epsilon1, double dampingfactor, int64_t maxiter, double threshold1);
+#if TEST_EPSLION_THRESHOLD == 1
+int64_t page_rank_directed(stinger_t * S, int64_t NV, double * pr, double * tmp_pr_in, double epsilon, double dampingfactor, int64_t maxiter, double threshold);
+#else
+int64_t page_rank_directed(stinger_t * S, int64_t NV, double * pr, double * tmp_pr_in, double epsilon, double dampingfactor, int64_t maxiter);
+#endif
 int64_t page_rank (stinger_t * S, int64_t NV, double * pr, double * tmp_pr_in, double epsilon, double dampingfactor, int64_t maxiter);
 int64_t page_rank_type(stinger_t * S, int64_t NV, double * pr, double * tmp_pr_in, double epsilon, double dampingfactor, int64_t maxiter, int64_t type);
 int64_t page_rank_type_directed(stinger_t * S, int64_t NV, double * pr, double * tmp_pr_in, double epsilon, double dampingfactor, int64_t maxiter, int64_t type);
@@ -61,8 +66,12 @@ static double threshold;
 static double epsilonchange;
 
 static struct stinger * S;
+static double * update_time_trace;
 
-FILE* fp;
+FILE* fp; //count
+FILE* fp_pr; //pr values
+int   fp_idx = 0;
+
 
 int
 main (const int argc, char *argv[])
@@ -203,9 +212,7 @@ main (const int argc, char *argv[])
   double dampingfactor = DAMPINGFACTOR_DEFAULT;
   int64_t maxiter = MAXITER_DEFAULT;
 
-#if COUNT_NUM_V_DELTA == 1
-  fp = fopen("delta_count.txt", "a");
-
+#if TEST_EPSLION_THRESHOLD == 1
   parse_args_1 (argc, argv, &initial_graph_name, &action_stream_name, &batch_size, &nbatch, &threshold, &epsilonchange);
   STATS_INIT();
 
@@ -223,30 +230,174 @@ main (const int argc, char *argv[])
   print_initial_graph_stats (nv, ne, batch_size, nbatch, naction);
   BATCH_SIZE_CHECK();
 
-  //page_rank_directed(stinger_t * S, int64_t NV, double * pr, double * tmp_pr_in, double epsilon, double dampingfactor, int64_t maxiter)
-
   double *pr = (double*)xmalloc(sizeof(double) * nv);
   double *tmp_pr = (double*)xmalloc(sizeof(double) * nv);
 
-  //page_rank_directed(S, nv, pr, tmp_pr, epsilon, dampingfactor, maxiter);
   tic ();
   S = stinger_new ();//added
   stinger_set_initial_edges (S, nv, 0, off, ind, weight, NULL, NULL, -2);//added
   PRINT_STAT_DOUBLE ("time_stinger", toc ());
-
+#if 1
 #if COUNT_NUM_V_DELTA == 1
-  LOG_I_A("threshold: %5.10e, epsilon: %5.10e", threshold, epsilonchange);
-  double epsilonchange1 = pow(10, -(epsilonchange));
-  double threshold1 = threshold * 0.0005;
-  LOG_I_A("threshold1: %e, epsilon1: %e", threshold1, epsilonchange1);
-  page_rank_directed(S, nv, pr, tmp_pr, epsilonchange1, dampingfactor, maxiter, threshold1);
+  fp = fopen("delta_count.txt", "a");
+#endif
+
+#if TEST_EPSLION_THRESHOLD == 1
+  LOG_I_A("threshold: %5.5e, epsilon: %5.5e", threshold, epsilonchange);
+  epsilonchange = pow(10, -(epsilonchange));
+  threshold = threshold * 0.000005;// 0.0005;
+  LOG_I_A("threshold: %5.5e, epsilon: %5.5e", threshold, epsilonchange);
+  page_rank_directed(S, nv, pr, tmp_pr, epsilonchange, dampingfactor, maxiter, threshold);
 #else
   page_rank_directed(S, nv, pr, tmp_pr, epsilon, dampingfactor, maxiter);
+#endif
+#endif
+
+#if ADD_GRAPH_UPDATE == 1
+  update_time_trace = xmalloc (nbatch * sizeof(*update_time_trace));
+
+  /* Convert to STINGER */
+  tic ();
+  //S = stinger_new ();
+  //stinger_set_initial_edges (S, nv, 0, off, ind, weight, NULL, NULL, -2);
+  PRINT_STAT_DOUBLE ("time_stinger", toc ());
+  fflush(stdout);
+
+  free(graphmem);
+
+  tic ();
+  uint32_t errorCode = stinger_consistency_check (S, nv);
+  double time_check = toc ();
+  PRINT_STAT_HEX64 ("error_code", (long unsigned) errorCode);
+  PRINT_STAT_DOUBLE ("time_check", time_check);
+
+  /* Updates */
+  int64_t ntrace = 0;
+
+#if 0
+  stinger_insert_edge (S, 0, 1, 2, 1, 2);
+  stinger_insert_edge (S, 0, 2, 1, 1, 2);
+#else
+
+  for (int64_t actno = 0; actno < nbatch * batch_size; actno += batch_size)
+  {
+    tic();
+    const int64_t endact = (actno + batch_size > naction ? naction : actno + batch_size);
+    int64_t *actions = &action[2*actno];
+    int64_t numActions = endact - actno;
+
+//    MTA("mta assert parallel")
+//    MTA("mta block dynamic schedule")
+//    OMP("omp parallel for")
+//	for(uint64_t k = 0; k < endact - actno; k++) {
+	for(uint64_t k = 0; k < 1; k++) {
+	  printf("----k: %" PRId64 "\n", k);
+	  //printf("----endact - actno: %" PRId64 "\n", endact - actno);
+	  //const int64_t i = actions[2 * k];
+      //const int64_t j = actions[2 * k + 1];
+	  int64_t i = actions[2 * k];
+      int64_t j = actions[2 * k + 1];
+      //printf("----i: %" PRId64 "\n", i);
+     // printf("----j: %" PRId64 "\n", j);
+
+      //stinger/stinger_core/src/stinger.c
+      //because of --> nv    = (3*nv)/4;
+      if (i > nv * 0.75) i = nv * 0.7;
+      if (j > nv * 0.75) j = nv * 0.7;
+#if 1
+      if (i != j && i < 0) {
+	     stinger_remove_edge(S, 0, ~i, ~j);
+	     stinger_remove_edge(S, 0, ~j, ~i);
+      }
+#endif
+      if (i != j && i >= 0) {
+	     stinger_insert_edge (S, 0, i, j, 1, actno+2);
+	     stinger_insert_edge (S, 0, j, i, 1, actno+2);
+      }
+
+    }
+//    update_time_trace[ntrace] = toc();
+//    ntrace++;
+  } /* End of batch */
+#endif
+
+#if 0
+  /* Print the times */
+  double time_updates = 0;
+  for (int64_t k = 0; k < nbatch; k++) {
+    time_updates += update_time_trace[k];
+  }
+  PRINT_STAT_DOUBLE ("time_updates", time_updates);
+  PRINT_STAT_DOUBLE ("updates_per_sec", (nbatch * batch_size) / time_updates);
+
+  tic ();
+  errorCode = stinger_consistency_check (S, nv);
+  time_check = toc ();
+  PRINT_STAT_HEX64 ("error_code", (long unsigned) errorCode);
+  PRINT_STAT_DOUBLE ("time_check", time_check);
+
+  free(update_time_trace);
+#endif
+//  stinger_free_all (S);
+//  free (actionmem);
+#endif //end of update
+
+
+#if 1
+  //initialize pr, tmp_pr
+  free(tmp_pr);
+  free(pr);
+  pr = (double*)xmalloc(sizeof(double) * nv);
+  tmp_pr = (double*)xmalloc(sizeof(double) * nv);
+  //call pagerank again
+#if TEST_EPSLION_THRESHOLD == 1
+  LOG_I_A("threshold: %5.5e, epsilon: %5.5e", threshold, epsilonchange);
+  page_rank_directed(S, nv, pr, tmp_pr, epsilonchange, dampingfactor, maxiter, threshold);
+#else
+  page_rank_directed(S, nv, pr, tmp_pr, epsilon, dampingfactor, maxiter);
+#endif
+#endif
+
+#if COUNT_NUM_V_DELTA == 1
+  fclose(fp);
 #endif
   STATS_END ();
   free(tmp_pr);
   free(pr);
 
+#if SAVE_PR_VALUES == 1
+//pr value comparison
+  fp =    fopen("pr_values_0.txt", "r");
+  fp_pr = fopen("pr_values_1.txt", "r");
+
+  if (fp == NULL || fp_pr == NULL)
+  {
+	  printf("ERROR: files are not exists!\n");
+	  exit(1);
+  }
+
+  double val1, val2;
+  double diff_epsilon = 1e-8;
+  int diff_count = 0;
+  //int num = 0;
+  while (!feof(fp) && !feof(fp_pr))
+  //if (num == 0)
+  {
+
+	  fscanf(fp, "%lf", &val1);
+	  fscanf(fp_pr, "%lf", &val2);
+	  //printf("val1: %e, val2: %e\n", val1, val2);
+	  //printf("val1-val2: %e\n", fabs(val1-val2));
+	  if (fabs(val1-val2) > diff_epsilon) diff_count++;
+#if 0
+	  printf("val1: %e, val2: %e\n", val1, val2);
+	  printf("num: %d\n", num++);
+//#else
+	  diff_count++;
+	  printf("diff_count: %d\n", diff_count);
+#endif
+  }
+  printf("diff_count: %d\n", diff_count);
   fclose(fp);
 #endif
 }
@@ -349,30 +500,63 @@ page_rank_subset(stinger_t * S, int64_t NV, uint8_t * vertex_set, int64_t vertex
 #endif
 
 int64_t
-//page_rank_directed(stinger_t * S, int64_t NV, double * pr, double * tmp_pr_in, double epsilon, double dampingfactor, int64_t maxiter) {
-page_rank_directed(stinger_t * S, int64_t NV, double * pr, double * tmp_pr_in, double epsilon1, double dampingfactor, int64_t maxiter, double threshold1) {
-
-  double * tmp_pr = set_tmp_pr(tmp_pr_in, NV);
+#if TEST_EPSLION_THRESHOLD == 1
+page_rank_directed(stinger_t * S, int64_t NV, double * pr, double * tmp_pr, double epsilon, double dampingfactor, int64_t maxiter, double threshold) {
+#else
+page_rank_directed(stinger_t * S, int64_t NV, double * pr, double * tmp_pr, double epsilon, double dampingfactor, int64_t maxiter) {
+#endif
+ // double * tmp_pr = set_tmp_pr(tmp_pr_in, NV);
 
   int64_t * pr_lock = (int64_t *)xcalloc(NV,sizeof(double));
+
+  for(uint64_t v = 0; v < NV; v++) { //pr[] initialization
+    pr[v] = 1 / ((double)NV);
+  }
 
   int64_t iter = maxiter;
   double delta = 1;
   int64_t iter_count = 0;
 
+#if SAVE_PR_VALUES == 1
+  LOG_I_A("!!!!!!!!!!!!!!!!called %d \n", fp_idx);
+  char buff[50];
+  sprintf(buff, "pr_values_%d.txt", fp_idx++);
+  fp_pr = fopen(buff, "w");
+#endif
+
+#if TEST_EPSLION_THRESHOLD == 1
   LOG_I_A("NV : %ld", NV);
-  LOG_I_A("threshold: %5.10e, epsilon: %5.10e", threshold1, epsilon1);
+  LOG_I_A("threshold: %5.10e, epsilon: %5.10e", threshold, epsilon);
+#endif
 
-  while (delta > epsilon1 && iter > 0) {
+#if NO_ITERATION_LIMIT == 1
+  while (delta > epsilon) { //no limit for iteration
+#else
+  while (delta > epsilon && iter > 0) {
+#endif
+
     iter_count++;
-
     double pr_constant = 0.0;
+
+    printf("iter_count: %d\n", iter_count);
 
 //    OMP("omp parallel for reduction(+:pr_constant)")
     for (uint64_t v = 0; v < NV; v++) {
       tmp_pr[v] = 0;
       if (stinger_outdegree(S,v) == 0) {
+#if 0
+        if (v == NV - 1)
+        {
+          printf("pr[%ld]:%5.5e, ", v, pr[v]);
+        }
+#endif
         pr_constant += pr[v];
+#if 0
+        if (v == NV - 1)
+        {
+       	  printf("pr_constant: %5.5e\n", pr_constant);
+        }
+#endif
       }
     }
     //LOG_I_A("pr_constant : %20.15e", pr_constant);
@@ -385,13 +569,31 @@ page_rank_directed(stinger_t * S, int64_t NV, double * pr, double * tmp_pr_in, d
       writeef(&pr_lock[STINGER_EDGE_DEST],count+1);
     } STINGER_PARALLEL_FORALL_EDGES_OF_ALL_TYPES_END();
 
+#if 0
+ if (iter_count == 10)
+ {
+	 for (uint64_t v = 1000; v < 1010; v++) {
+		printf("tmp_pr[%d]  = %5.5e\n", v, tmp_pr[v]);
+		printf("pr_lock[%d] = %5.5e\n", v, pr_lock[v]);
+	 }
+ }
+#endif
+
 //    OMP("omp parallel for")
     for (uint64_t v = 0; v < NV; v++) {
-      tmp_pr[v] = (tmp_pr[v] + pr_constant / (double)NV) * dampingfactor + (((double)(1-dampingfactor)) / ((double)NV));
-    }
 
+      if (v == 0)
+      {
+    	  printf("tmp_pr[%d]  = %5.5e, pr_constant: %5.5e, pr_constant / (double)NV: %5.5e\n", v, tmp_pr[v], pr_constant, pr_constant / (double)NV);
+      }
+      tmp_pr[v] = (tmp_pr[v] + pr_constant / (double)NV) * dampingfactor + (((double)(1-dampingfactor)) / ((double)NV));
+      //tmp_pr[v] = (tmp_pr[v]) * dampingfactor + (((double)(1-dampingfactor)) / ((double)NV));
+    }
+#if COUNT_NUM_V_DELTA == 1
     delta = 0;
     int count = 0;
+#endif
+
 //    OMP("omp parallel for reduction(+:delta)")
     for (uint64_t v = 0; v < NV; v++) {
       double mydelta = tmp_pr[v] - pr[v];
@@ -399,22 +601,37 @@ page_rank_directed(stinger_t * S, int64_t NV, double * pr, double * tmp_pr_in, d
         mydelta = -mydelta;
       delta += mydelta;
       double change = mydelta/pr[v];
-      if ( change > threshold1 ) count++;
+#if COUNT_NUM_V_DELTA == 1
+      if ( change > threshold ) count++;
+#endif
     }
+#if COUNT_NUM_V_DELTA == 1
     //LOG_I_A("delta : %20.15e", delta);
     //LOG_I_A("count : %d", count);
     fprintf(fp, "%d ", count);
+#endif
 
 //    OMP("omp parallel for")
     for (uint64_t v = 0; v < NV; v++) {
       pr[v] = tmp_pr[v];
     }
 //    LOG_I_A("iter : %ld", iter);
-    iter--;
+#if NO_ITERATION_LIMIT != 1
+    iter--; //no limit for iteration
+#endif
   }
-
+#if COUNT_NUM_V_DELTA == 1
   fprintf(fp, "\n");
-  unset_tmp_pr(tmp_pr,tmp_pr_in);
+#endif
+
+#if SAVE_PR_VALUES == 1
+  for (uint64_t v = 0; v < NV; v++) {
+    fprintf(fp_pr, "%e ", pr[v]);
+    if (((v+1) % 10) == 0) fprintf(fp_pr, "\n");
+  }
+  fclose(fp_pr);
+#endif
+  //unset_tmp_pr(tmp_pr,tmp_pr_in);
   xfree(pr_lock);
 }
 
